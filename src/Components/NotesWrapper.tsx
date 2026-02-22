f4dd596c8f19280da65e045047afe18e2aa5a943
devsuheyb <devsuheyb@gmail.com>
2026-02-22 09:42:05 +0300

    addNote function added

--- src/Components/NotesWrapper.tsx
+++ src/Components/NotesWrapper.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import NotesForm from "./NotesForm";
+import ListOfNotes from "./ListOfNotes";
+import EditNotes from "./EditNotes";
+import Search from "./Search";
 
-const NotesWrapper = () => {
-  return (
-    <div>NotesWrapper</div>
-  )
+interface NotesType {
+  id: number;
+  title: string;
+  message: string;
+  createdAt: number;
+  updatedAt: number;
+  favorite: boolean;
+  time: string;
+  isEditing: boolean;
 }
+const NotesWrapper = () => {
+  const [notes, setNotes] = useState<NotesType[]>([]);
 
-export default NotesWrapper
+  // time 'ago' calculated here
+
+  // adding Notes function
+  const addNotes = (title: string, message: string) => {
+    const timeStamp = Date.now();
+    setNotes((prevNotes) => [
+      ...prevNotes,
+      {
+        id: timeStamp,
+        title,
+        message,
+        createdAt: timeStamp,
+        updatedAt: timeStamp,
+        favorite: false,
+        time: "",
+        isEditing: false,
+      },
+    ]);
+  };
+  return (
+    <div className="flex justify-center mt-20">
+      <div className="w-200   flex flex-col items-center  gap-5">
+        <div className="border w-full">
+          <div className="">
+            <div className="flex justify-end pr-5 pt-5">
+              <Search />
+            </div>
+
+            <div className="flex justify-center w-full ">
+              <NotesForm addNotes={addNotes} />
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+
+        <div className="w-full border ">
+          {/* this is where notes goes */}
+          <ListOfNotes />
+          <EditNotes />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotesWrapper;
